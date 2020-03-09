a1c27f81cc4ae98a970e96ccfa9fb3c0b2e25c25
cunhamuril <murilo.sant@hotmail.com>
2020-03-09 17:25:44 -0300

    feat(header): add header

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Header from './components/Header'
+
+import GlobalStyle from './styles/global'
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const HeaderComponent = styled.header`
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 62px;
+  background-color: #009CA3;
+`
