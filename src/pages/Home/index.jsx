6ac48626f6dc53affca821dc09ebb8a0f1ff8bf5
cunhamuril <murilo.sant@hotmail.com>
2020-03-10 17:36:33 -0300

    style(restaurantcards): restaurantCards

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import arrayChunk from "lodash.chunk";
 
 import SearchField from '../../components/SearchField'
 import RestaurantCard from './components/RestaurantCard'
@@ -22,7 +23,7 @@ const Home = () => {
       key: 2,
       name: 'Nome do restaurante',
       address: 'Endereço do restaurante',
-      isOpen: false,
+      isOpen: true,
       thumbnail: RestaurantLogo
     },
     {
@@ -31,21 +32,67 @@ const Home = () => {
       address: 'Endereço do restaurante',
       isOpen: true,
       thumbnail: RestaurantLogo
-    }
+    },
+    {
+      key: 4,
+      name: 'Nome do restaurante',
+      address: 'Endereço do restaurante',
+      isOpen: false,
+      thumbnail: RestaurantLogo
+    },
+    {
+      key: 5,
+      name: 'Nome do restaurante',
+      address: 'Endereço do restaurante',
+      isOpen: false,
+      thumbnail: RestaurantLogo
+    },
+    {
+      key: 6,
+      name: 'Nome do restaurante',
+      address: 'Endereço do restaurante',
+      isOpen: true,
+      thumbnail: RestaurantLogo
+    },
+    {
+      key: 7,
+      name: 'Nome do restaurante',
+      address: 'Endereço do restaurante',
+      isOpen: false,
+      thumbnail: RestaurantLogo
+    },
+    {
+      key: 8,
+      name: 'Nome do restaurante',
+      address: 'Endereço do restaurante',
+      isOpen: false,
+      thumbnail: RestaurantLogo
+    },
+    {
+      key: 9,
+      name: 'Nome do restaurante',
+      address: 'Endereço do restaurante',
+      isOpen: true,
+      thumbnail: RestaurantLogo
+    },
   ]
+
+  const chukedData = arrayChunk(restaurants, 3)
 
   return (
     <div className='home'>
       <h2 className='mt-5 d-flex justify-content-center'>Bem-vindo ao Lista Rango</h2>
       <SearchField backgroundColor='#FBFBFB' textLabel='Buscar estabelecimento' />
-      <Container fluid className='d-flex align-items-center justify-content-center mt-5'>
-        <Row>
-          {restaurants.map(item => (
-            <Col sm='12' md='6' lg='4' key={item.key}>
-              <RestaurantCard restaurant={item} />
-            </Col>
-          ))}
-        </Row>
+      <Container fluid className='d-flex flex-column align-items-center mt-5'>
+        {chukedData.map((row, rowIndex) => (
+          <Row key={rowIndex}>
+            {row.map(restaurant => (
+              <Col sm='12' md='6' lg='4' key={restaurant.key}>
+                <RestaurantCard restaurant={restaurant} />
+              </Col>
+            ))}
+          </Row>
+        ))}
       </Container>
     </div>
   )
